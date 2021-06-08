41f9fde5e6f55cfd5020531a106202d9fcd5c37e
Alastair M. Robinson <robinsonb5@gmail.com>
2021-06-08 19:23:01 +0100

    Added supporting files for demistify framework.

---
+++ firmware/overrides.c
@@ -0,0 +1,27 @@
+#include "keyboard.h"
+
+/* Key -> gamepad mapping.  We override this to swap buttons A and B for NES. */
+
+unsigned char joy_keymap[]=
+{
+	KEY_CAPSLOCK,
+	KEY_LSHIFT,
+	KEY_LCTRL,
+	KEY_ALT,
+	KEY_W,
+	KEY_S,
+	KEY_A,
+	KEY_D,
+	KEY_ENTER,
+	KEY_RSHIFT,
+	KEY_RCTRL,
+	KEY_ALTGR,
+	KEY_UPARROW,
+	KEY_DOWNARROW,
+	KEY_LEFTARROW,
+	KEY_RIGHTARROW,
+};
+
+/* Initial ROM */
+const char *bootrom_name="AUTOBOOTNES";
+
